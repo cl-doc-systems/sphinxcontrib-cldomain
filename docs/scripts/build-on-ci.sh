f3f322ccc4ff8684849dd6c3575a73b3e4874743
Alexander Artemenko <svetlyak.40wt@gmail.com>
2020-11-01 00:00:23 +0300

    Added scripts to build docs.

---
+++ docs/scripts/build-on-ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# we only want to build documentation from the "main" branch
+# and want to make it only once.
+# Also, we need to check if TRAVIS_PULL_REQUEST=false because Travis
+# builds which are running for pull requests will have
+# TRAVIS_BRANCH=main, but TRAVIS_PULL_REQUEST_BRANCH=the-branch
+# and TRAVIS_PULL_REQUEST=42 where 42 is a pull request number
+if [ "$TRAVIS_BRANCH" = "main" -a "$TRAVIS_PULL_REQUEST" = "false" -a "$LISP" = "ccl" ]; then
+    sudo pip install pipenv
+    pipenv install
+    pipenv run ./build-docs.ros
+else
+    echo "Skipping documentation build because environment is not suitable."
+fi
